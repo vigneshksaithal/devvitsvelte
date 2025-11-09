ecc5dbcfc239b0c298289c292bc3bfc5fe555d4e
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2025-11-09 15:06:56 +0530

    style: format code and improve readability in various files, including App.svelte and tsconfig.json

--- src/client/App.svelte
+++ src/client/App.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
-  const title = $state('Devvit + Svelte')
+	const title = $state('Devvit + Svelte')
 </script>
 
 <main>
-  <h1>{title}</h1>
+	<h1>{title}</h1>
+	{#if title === 'Devvit + Svelte'}
+		<p>This is a test</p>
+	{:else}
+		<p>This is not a test</p>
+	{/if}
 </main>
