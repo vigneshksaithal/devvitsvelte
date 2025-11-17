a3790ed5cae1168cad11b3737d977107ff276b12
Vignesh Aithal <60360863+vigneshksaithal@users.noreply.github.com>
2025-11-17 12:39:58 +0530

    fix: update post title template syntax from '<% name %>' to '{{ name }}'

--- src/server/post.ts
+++ src/server/post.ts
@@ -7,10 +7,7 @@ export const createPost = async () => {
   }
 
   return await reddit.submitCustomPost({
-    splash: {
-      appDisplayName: '<% name %>'
-    },
     subredditName,
-    title: '<% name %>'
+    title: '{{ name }}'
   })
 }
